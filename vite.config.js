2fd7419b13a81673be5606b10002f6f7a52397cd
bb <bb@attacker.com>
2026-07-28 11:23:32 -0400

    chore: configure vite for multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Char0n.io/'
+    base: '/Char0n.io/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                advisory: resolve(__dirname, 'advisory.html')
+            }
+        }
+    }
 })
